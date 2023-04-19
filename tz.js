6165d110d0ea4ac8926d430ff0aedb20321d29a0
BrandonSkyTai <bt325sky@gmail.com>
2023-04-18 20:36:57 -0400

    Added format

--- tz.js
+++ tz.js
@@ -1,14 +1,15 @@
 const moment = require('moment-timezone');
+const yargs = require('yargs');
 
 moment.tz.setDefault('America/New_York');
+let formatstr = '';
 
-let targetTimezone;
+const args = yargs
+  .usage('Usage: node <script-file> <timezone>')
+  .demandCommand(1, 'Please provide a timezone')
+  .argv;
 
-if (process.argv.length != 3) {
-  console.log('Usage: node <script-file> <timezone>');
-  process.exit(1);
-} else {
-  targetTimezone = process.argv[2];
-}
+const targetTimezone = args._[0];
+if (yargs.argv.format) { formatstr = 'dddd, MMMM do YYYY, h:mm:ss a'; }
 
-console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
+console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format(formatstr)}`);
